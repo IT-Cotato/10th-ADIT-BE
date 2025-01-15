c944b543dbc372533c8a55ffa2b3e58df77dee99
YoungjaeRo <ryj0330@naver.com>
2025-01-15 15:10:52 +0900

    etc: 패키지명 변경

--- src/main/java/com/adit/backend/domain/scraper/controller/scraperController.java
+++ src/main/java/com/adit/backend/domain/scraper/controller/scraperController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.adit.backend.domain.scraper.Service.scraperService;
+import com.adit.backend.domain.scraper.service.scraperService;
 import com.adit.backend.domain.scraper.dto.response.scraperResponse;
 import com.adit.backend.global.common.ApiResponse;
 import lombok.AccessLevel;
