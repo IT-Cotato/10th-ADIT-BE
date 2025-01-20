1a6ce24df9b89ddef623bc7308ecddaad291e606
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-01-21 00:27:09 +0900

    :bug: fix: CORS 설정 오류 해결 (#40) - `addAllowedOrigin` & `setAllowCredentials` 동시 사용 보안 문제 해결

--- src/main/java/com/adit/backend/global/config/CorsConfig.java
+++ src/main/java/com/adit/backend/global/config/CorsConfig.java
@@ -11,21 +11,23 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
+	public static final String SERVER_URL = "http://ec2-3-39-228-156.ap-northeast-2.compute.amazonaws.com:8080";
+	public static final String FRONT_URL = "http://localhost:3000";
+
 	@Bean
 	public CorsFilter corsFilter() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*");
+		config.addAllowedOrigin(SERVER_URL);
+		config.addAllowedOrigin(FRONT_URL);
 		config.addAllowedHeader("*");
 		config.addAllowedMethod("*");
-		config.setExposedHeaders(List.of(
-			"Authorization",
-			"accessToken",
-			"refreshToken",
-			"Set-Cookie"
-		));
+		config.setExposedHeaders(List.of("accessToken"));
+		config.addExposedHeader("Set-Cookie");
 		config.setMaxAge(3600L);
+
 		source.registerCorsConfiguration("/api/**", config);
 		return new CorsFilter(source);
 	}
