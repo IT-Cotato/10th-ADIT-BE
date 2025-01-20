0ba9465187e1b1c4afbd2e465094cd1b3910da84
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-01-20 23:46:26 +0900

    :recycle: refactor: Apify API 호출 코드를 크롤링 전략 내부에 포함 (#38)

--- src/main/java/com/adit/backend/infra/crawler/platform/InstagramCrawlingStrategy.java
+++ src/main/java/com/adit/backend/infra/crawler/platform/InstagramCrawlingStrategy.java
@@ -1,27 +1,33 @@
-package com.adit.backend.domain.scraper.service;
+package com.adit.backend.infra.crawler.platform;
 
 import static com.adit.backend.global.error.GlobalErrorCode.*;
 
+import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import com.adit.backend.domain.scraper.dto.response.scraperResponse;
-import com.adit.backend.domain.scraper.exception.scraperException;
+
+import com.adit.backend.domain.ai.dto.response.CrawlCompletionResponse;
+import com.adit.backend.infra.crawler.common.AbstractWebCrawlingStrategy;
+import com.adit.backend.infra.crawler.exception.CrawlingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
-@Service
+@Component
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
-public class scraperService {
+public class InstagramCrawlingStrategy extends AbstractWebCrawlingStrategy {
 
 	private final RestTemplate restTemplate;
+	private static final String INSTAGRAM_URL = "instagram.com";
 
 	//API 요청을 위한 토큰
 	@Value("${apify.token}")
@@ -36,7 +42,19 @@ public class scraperService {
 	@Value("${apify.output-url}")
 	private String baseOutputUrl;
 
-	public scraperResponse.scarperInfoDto executeScraper(String targetUrl) {
+	@Override
+	public boolean supports(String url) {
+		return url.contains(INSTAGRAM_URL);
+	}
+
+	@Override
+	public CrawlCompletionResponse extractContents(Document document) {
+		throw new UnsupportedOperationException("Jsoup을 이용한 크롤링이 불가능합니다.");
+	}
+
+	@Override
+	@Cacheable(value = "contentCache", key = "caption")
+	public CrawlCompletionResponse extractContentsUsingApify(String targetUrl) {
 		try {
 			// Apify 작업 실행 URL 생성 및 요청 데이터 준비
 			String startTaskUrl = createStartTaskUrl();
@@ -55,10 +73,9 @@ public class scraperService {
 			String caption = getCaptionFromOutput(outputResponse);
 
 			// caption 반환
-			return new scraperResponse.scarperInfoDto(caption);
-
+			return CrawlCompletionResponse.of(caption, null);
 		} catch (Exception e) {
-			throw new scraperException(SCRAPER_API_FAILED);
+			throw new CrawlingException(SCRAPER_API_FAILED);
 		}
 	}
 
@@ -86,7 +103,7 @@ public class scraperService {
 		JsonNode responseJson = objectMapper.readTree(response.getBody());
 		JsonNode dataNode = responseJson.path("data");
 		if (dataNode.isMissingNode() || !dataNode.has("defaultDatasetId")) {
-			throw new scraperException(FIELD_NOT_FOUND);
+			throw new CrawlingException(FIELD_NOT_FOUND);
 		}
 
 		return dataNode.get("defaultDatasetId").asText();
@@ -112,9 +129,10 @@ public class scraperService {
 	private String getCaptionFromOutput(JsonNode outputResponse) {
 		JsonNode firstPost = outputResponse.get(0);
 		if (!firstPost.has("caption")) {
-			throw new scraperException(FIELD_NOT_FOUND);
+			throw new CrawlingException(FIELD_NOT_FOUND);
 		}
 
 		return firstPost.get("caption").asText();
 	}
+
 }
