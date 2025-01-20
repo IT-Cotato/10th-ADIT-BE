bab787cd9d699d38da4ce388f74c02b83a50232a
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-01-20 23:49:30 +0900

    :recycle: refactor: Instagram 플랫폼 추가로 인한 비즈니스 로직 수정 (#38) - 플랫폼이 Instagram 일 경우 수정한 Apify API 호출하여 AI 요약 요청 호출 - 그외 플랫폼은 기존에 정의한 로직을 수행

--- src/main/java/com/adit/backend/infra/crawler/common/WebCrawlingStrategy.java
+++ src/main/java/com/adit/backend/infra/crawler/common/WebCrawlingStrategy.java
@@ -14,4 +14,6 @@ public interface WebCrawlingStrategy {
 	Document getDocument(String url) throws IOException;
 
 	CrawlCompletionResponse extractContents(Document document);
+
+	CrawlCompletionResponse extractContentsUsingApify(String url);
 }
--- src/main/java/com/adit/backend/infra/crawler/service/WebContentCrawlingService.java
+++ src/main/java/com/adit/backend/infra/crawler/service/WebContentCrawlingService.java
@@ -11,18 +11,19 @@ import com.adit.backend.domain.ai.dto.response.CrawlCompletionResponse;
 import com.adit.backend.global.error.GlobalErrorCode;
 import com.adit.backend.infra.crawler.common.WebCrawlingStrategy;
 import com.adit.backend.infra.crawler.exception.CrawlingException;
+import com.adit.backend.infra.crawler.platform.InstagramCrawlingStrategy;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-
 @Service
 @Slf4j
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class WebContentCrawlingService {
+	private static final String INSTAGRAM_URL = "instagram.com";
 	private final List<WebCrawlingStrategy> crawlingStrategies;
-
+	private final InstagramCrawlingStrategy instagramCrawlingStrategy;
 	/**
 	 * 크롤링 비동기 처리
 	 */
@@ -30,15 +31,22 @@ public class WebContentCrawlingService {
 	public CompletableFuture<CrawlCompletionResponse> crawlAsync(String url) {
 		try {
 			WebCrawlingStrategy strategy = findStrategy(url);
-			Document document = strategy.getDocument(url);
-			CrawlCompletionResponse contents = strategy.extractContents(document);
-			log.info("[크롤링 완료] : {}", url);
+			CrawlCompletionResponse contents;
+			if (url.contains(INSTAGRAM_URL)) {
+				contents = strategy.extractContentsUsingApify(url);
+				log.info("[Instagram 크롤링 완료] : {}", url);
+			} else {
+				Document document = strategy.getDocument(url);
+				contents = strategy.extractContents(document);
+				log.info("[플랫폼 크롤링 완료] : {}", url);
+			}
 			return CompletableFuture.completedFuture(contents);
 		} catch (Exception e) {
-			log.error("[크롤링 실패] : {}", url);
-			throw new CrawlingException(GlobalErrorCode.CRAWLING_FAILED);
+			log.error("[크롤링 실패] : {}, 오류 메시지: {}", url, e.getMessage(), e);
+			return CompletableFuture.failedFuture(new CrawlingException(GlobalErrorCode.CRAWLING_FAILED));
 		}
 	}
+
 
 	/**
 	 * 플랫폼 구별
