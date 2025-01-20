3f8acfa6e9b08dcb544b1f9a208c3f4bc2b17355
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-01-20 18:44:53 +0900

    :fire: remove: 미사용 코드 제거 (#13)

--- src/main/java/com/adit/backend/domain/ai/dto/response/ContentResponse.java
+++ src/main/java/com/adit/backend/domain/ai/dto/response/ContentResponse.java
@@ -1,13 +1,12 @@
 package com.adit.backend.domain.ai.dto.response;
 
 import com.adit.backend.domain.ai.enums.ContentType;
+
+
 public record ContentResponse(
 	String name,
 	ContentType type,
 	String location,
 	String period
 ) {
-	public static ContentResponse of(String name, String location, ContentType type, String period) {
-		return new ContentResponse(name, type, location, period);
-	}
 }
