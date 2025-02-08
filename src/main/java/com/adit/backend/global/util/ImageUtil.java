eed81721543814c59d70c4160218a0abd7dfae6d
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-02-09 04:31:29 +0900

    :sparkles: feat: 이미지 업로드 및 업데이트 비동기 처리 추가, 디렉토리 enum 생성 (#68)

--- src/main/java/com/adit/backend/global/util/ImageUtil.java
+++ src/main/java/com/adit/backend/global/util/ImageUtil.java
@@ -1,22 +1,10 @@
 package com.adit.backend.global.util;
 
-import static com.adit.backend.global.error.GlobalErrorCode.*;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-
-import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.adit.backend.domain.image.exception.ImageException;
+import java.util.UUID;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -27,41 +15,7 @@ public class ImageUtil {
 	public static final String HTTPS = "https://";
 	public static final String HTTP = "http://";
 
-	/**
-	 * 주어진 이미지 URL을 읽어 MultipartFile로 변환하여 반환한다.
-	 *
-	 * @param imageUrl 이미지의 URL 문자열
-	 * @return 변환된 MultipartFile 객체
-	 * @throws Exception 이미지 읽기 및 변환 중 예외 발생 시
-	 */
-	// URL로부터 데이터를 읽어와서 CustomMultipartFile로 변환하여 반환
-	public static MultipartFile convertUrlToMultipartFile(String imageUrl) {
-		try {
-			// URL에 프로토콜이 누락된 경우 "https://"를 추가함
-			String normalizedUrl = normalizeUrl(imageUrl);
-			URL url = new URL(normalizedUrl);
-			URLConnection connection = url.openConnection();
-			connection.setConnectTimeout(5000);
-			connection.setReadTimeout(5000);
-			String contentType = connection.getContentType();
-
-			String fileName = extractFileName(normalizedUrl);
-			byte[] content;
-
-			try (InputStream inputStream = connection.getInputStream();
-				 ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-				IOUtils.copy(inputStream, outputStream);
-				content = outputStream.toByteArray();
-			}
-
-			return new CustomMultipartFile(content, fileName, contentType);
-		} catch (IOException e) {
-			log.error("[Image] URL을 MultipartFile로 변환하는 데 실패했습니다.: {}", imageUrl);
-			throw new ImageException(IMAGE_EXTRACTION_FAILED);
-		}
-	}
-
-	private static String extractFileName(String fileUrl) {
+	public static String extractFileName(String fileUrl) {
 		try {
 			URL url = new URL(fileUrl);
 			String query = url.getQuery();
@@ -80,7 +34,7 @@ public class ImageUtil {
 		}
 	}
 
-	private static String normalizeUrl(String imageUrl) {
+	public static String normalizeUrl(String imageUrl) {
 		if (imageUrl.contains(FILE_NAME_PARAM)) {
 			String fileName = imageUrl.substring(imageUrl.indexOf(FILE_NAME_PARAM) + FILE_NAME_PARAM.length());
 			fileName = fileName.contains("&") ? fileName.substring(0, fileName.indexOf("&")) : fileName;
@@ -98,56 +52,41 @@ public class ImageUtil {
 		return imageUrl;
 	}
 
-	// MultipartFile 인터페이스를 구현한 커스텀 클래스
-	public static class CustomMultipartFile implements MultipartFile {
-		private final byte[] content;
-		private final String fileName;
-		private final String contentType;
-
-		public CustomMultipartFile(byte[] content, String fileName, String contentType) {
-			this.content = content;
-			this.fileName = fileName;
-			this.contentType = contentType;
+	public static String extractPathWithoutFileName(String oldKey) {
+		int lastSlashIndex = oldKey.lastIndexOf('/');
+		if (lastSlashIndex != -1) {
+			return oldKey.substring(0, lastSlashIndex);
 		}
+		return oldKey;
+	}
 
-		@Override
-		public String getName() {
-			return fileName;
+	// "."의 존재 유무만 판단 (잘못된 형식이면 S3Exception 발생)
+	public static String getFileExtension(String fileName, String contentType) {
+		int dotIndex = fileName.lastIndexOf(".");
+		if (dotIndex != -1) {
+			return fileName.substring(dotIndex);
+		} else {
+			if (contentType != null) {
+				switch (contentType.toLowerCase()) {
+					case "image/jpeg":
+						return ".jpg";
+					case "image/png":
+						return ".png";
+					case "image/gif":
+						return ".gif";
+					// 필요한 경우 추가 MIME 타입 처리
+					default:
+						log.warn("[S3] 인식되지 않은 MIME 타입: {}. 기본 확장자(.jpg)를 사용합니다.", contentType);
+						return ".jpg";
+				}
+			} else {
+				log.warn("[S3] 콘텐츠 타입을 확인할 수 없습니다.");
+				return ".jpg";
+			}
 		}
+	}
 
-		@Override
-		public String getOriginalFilename() {
-			return fileName;
-		}
-
-		@Override
-		public String getContentType() {
-			return contentType;
-		}
-
-		@Override
-		public boolean isEmpty() {
-			return content == null || content.length == 0;
-		}
-
-		@Override
-		public long getSize() {
-			return content.length;
-		}
-
-		@Override
-		public byte[] getBytes() {
-			return content;
-		}
-
-		@Override
-		public InputStream getInputStream() {
-			return new ByteArrayInputStream(content);
-		}
-
-		@Override
-		public void transferTo(File dest) throws IOException, IllegalStateException {
-			Files.write(dest.toPath(), content);
-		}
+	public static String createFileName(String originalFileName, String dirName, String contentType) {
+		return dirName + "/" + UUID.randomUUID() + getFileExtension(originalFileName, contentType);
 	}
 }
