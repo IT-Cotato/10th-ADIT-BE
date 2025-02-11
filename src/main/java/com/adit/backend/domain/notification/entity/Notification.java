6b48aaaa6da80e394de5a0b9b113cb7934007799
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-02-12 00:43:04 +0900

    :art: feat: 미사용 연관관계 제거 및 Notification 엔티티 구조 변경 (#64)

--- src/main/java/com/adit/backend/domain/notification/entity/Notification.java
+++ src/main/java/com/adit/backend/domain/notification/entity/Notification.java
@@ -1,42 +1,66 @@
 package com.adit.backend.domain.notification.entity;
 
-import com.adit.backend.domain.event.entity.UserEvent;
+import org.hibernate.annotations.ColumnDefault;
+
+import com.adit.backend.domain.notification.enums.NotificationType;
 import com.adit.backend.domain.user.entity.User;
 import com.adit.backend.global.entity.BaseEntity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Notification extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+	@NotNull
+	private String message;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "event_id")
-    private UserEvent userEvent;
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private NotificationType notificationType;
 
-    @Column(nullable = false)
-    private String content;
+	private String relatedUri;
 
-    private Boolean isRead;
+	@NotNull
+	@ColumnDefault("false")
+	private boolean isRead = false;
+
+	@NotNull
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Builder
+	public Notification(String message, NotificationType notificationType, String relatedUri) {
+		this.message = message;
+		this.notificationType = notificationType;
+		this.relatedUri = relatedUri;
+	}
+
+	//연관관계 메서드
+	public void assignUser(User user) {
+		this.user = user;
+	}
+
+	public void markAsRead() {
+		this.isRead = true;
+	}
 }
